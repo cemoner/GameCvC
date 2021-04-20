c6ac4759790f6847ea8da708b64ebee40ccad1d6
cemoner <2020556049@ogr.cu.edu.tr>
2021-04-20 20:47:42 +0300

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        XoxCvC cvc = new XoxCvC(25);
-        for(int i = 1;i<=1000;i++){
+        XoxCvC cvc = new XoxCvC(3);
+        for(int i = 1;i<=100;i++){
             cvc.game();
-            Thread.sleep(250);
+            Thread.sleep(50);
         }
         System.out.println(cvc.winC1);
         System.out.println(cvc.winC2);
